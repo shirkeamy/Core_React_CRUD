d8daedf12aa959188e68735b6e0ff3ff194c495b
Amol Shirke <shirkeamy@gmail.com>
2025-03-07 00:16:07 +0530

    Step 5 - inject repository service application

--- AmolShirkeCrudOperationsAPI/Program.cs
+++ AmolShirkeCrudOperationsAPI/Program.cs
@@ -1,4 +1,6 @@
+using AmolShirkeCrudOperationsAPI.Context;
 using AmolShirkeCrudOperationsAPI.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("StudentDbConnectionString"))
+);
 
 builder.Services.AddScoped<IStudentRepository, StudentRepository>();
 
