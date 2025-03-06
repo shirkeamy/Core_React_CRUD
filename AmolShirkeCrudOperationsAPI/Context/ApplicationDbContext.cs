20608302af10bbb72ab9c5b9f7bd5e034623b08f
Amol Shirke <shirkeamy@gmail.com>
2025-03-06 23:42:48 +0530

    Step 2 - Add Db context file

---
+++ AmolShirkeCrudOperationsAPI/Context/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using AmolShirkeCrudOperationsAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AmolShirkeCrudOperationsAPI.Context
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<State> States { get; set; }
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<StudentCourse> StudentCourses { get; set; }
+
+    }
+}
