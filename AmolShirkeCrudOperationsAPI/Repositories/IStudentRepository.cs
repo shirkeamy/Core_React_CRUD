809e30cd987c8d860a1514673ba18af7d44f354e
Amol Shirke <shirkeamy@gmail.com>
2025-03-06 23:54:18 +0530

    Step 3 - Add Repository into code

---
+++ AmolShirkeCrudOperationsAPI/Repositories/IStudentRepository.cs
@@ -0,0 +1,17 @@
+﻿using AmolShirkeCrudOperationsAPI.Models;
+
+namespace AmolShirkeCrudOperationsAPI.Repositories
+{
+    public interface IStudentRepository
+    {
+        Task<IEnumerable<Student>> GetAllStudentAsync();
+
+        Task<Student> GetStudentByIdAsync(int studentId);
+
+        Task AddStudentAsync(Student student);
+
+        Task DeleteStudentAsync(int StudentId);
+
+        Task UpdateStudentAsync(Student student);
+    }
+}
---
+++ AmolShirkeCrudOperationsAPI/Repositories/StudentRepository.cs
@@ -0,0 +1,46 @@
+﻿using AmolShirkeCrudOperationsAPI.Context;
+using AmolShirkeCrudOperationsAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AmolShirkeCrudOperationsAPI.Repositories
+{
+    public class StudentRepository: IStudentRepository
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+        public StudentRepository(ApplicationDbContext context) {
+            _applicationDbContext = context;
+        }
+
+        async Task<IEnumerable<Student>> IStudentRepository.GetAllStudentAsync()
+        {
+            return await _applicationDbContext.Students.ToListAsync();
+        }
+
+        async Task<Student> IStudentRepository.GetStudentByIdAsync(int studentId)
+        {
+            return await _applicationDbContext.Students.FindAsync(studentId);
+        }
+
+        async Task IStudentRepository.AddStudentAsync(Student student)
+        {
+            await _applicationDbContext.Students.AddAsync(student);
+            await _applicationDbContext.SaveChangesAsync();
+        }
+
+        async Task IStudentRepository.DeleteStudentAsync(int studentId)
+        {
+            var student = _applicationDbContext.Students.FindAsync(studentId);
+            if (student != null)
+            {
+                _applicationDbContext.Remove(student);
+                await _applicationDbContext.SaveChangesAsync();
+            }
+        }
+
+        async Task IStudentRepository.UpdateStudentAsync(Student student)
+        {
+            _applicationDbContext.Update(student);
+            await _applicationDbContext.SaveChangesAsync();
+        }
+    }
+}
